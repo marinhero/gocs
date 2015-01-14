dadeacad8b2ca1d9dea6f6a23c9ccd38d3fc435b
Marin Alcaraz <marin.alcaraz@gmail.com>
2015-01-14 17:55:54 -0500

    Working prompt. Good interaction btwn srv and cl

--- my_ftp.go
+++ my_ftp.go
@@ -3,26 +3,15 @@
 ** Author: Marin Alcaraz
 ** Mail   <marin.alcaraz@gmail.com>
 ** Started on  Mon Jan 12 17:22:29 2015 Marin Alcaraz
-** Last update Wed Jan 14 12:34:27 2015 Marin Alcaraz
+** Last update Wed Jan 14 17:32:12 2015 Marin Alcaraz
  */
 
 package main
 
 import (
 	"fmt"
-	"net"
 	"os"
 )
-
-func connect(s string, p string) (con net.Conn, err error) {
-	connectionString := s + ":" + string(p)
-	fmt.Println("Attempting to connect...")
-	conn, err := net.Dial("tcp", connectionString)
-	if err != nil {
-		return nil, fmt.Errorf("Dial error: %s", err)
-	}
-	return conn, nil
-}
 
 func main() {
 	arguments := os.Args[1:]
@@ -36,6 +25,10 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-		fmt.Println(interact(c))
+		err = interact(c)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
 }
--- net_utils.go
+++ net_utils.go
@@ -3,7 +3,7 @@
 ** Author: Marin Alcaraz
 ** Mail   <marin.alcaraz@gmail.com>
 ** Started on  Wed Jan 14 12:20:17 2015 Marin Alcaraz
-** Last update Wed Jan 14 12:25:46 2015 Marin Alcaraz
+** Last update Wed Jan 14 17:54:26 2015 Marin Alcaraz
  */
 
 package main
@@ -13,19 +13,42 @@ import (
 	"net"
 )
 
+func connect(s string, p string) (con net.Conn, err error) {
+	connectionString := s + ":" + string(p)
+	conn, err := net.Dial("tcp", connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("Dial error: %s", err)
+	}
+	return conn, nil
+}
+
 func interact(con net.Conn) error {
-	var message string
-	fmt.Println("Conected")
+	var request string
+	var err error
+
+	readed := 0
+	response := make([]byte, 8)
+
 	for {
-		fmt.Print("$> ")
-		_, err := fmt.Scanln(&message)
-		if message == "quit" {
-			break
+		//Read from server
+		readed, err = con.Read(response)
+		fmt.Print(string(response))
+		if readed != len(response) {
+			//Read request from keyboard
+			fmt.Print("ftp> ")
+			_, err = fmt.Scanf("%s", &request)
+			if request == "quit" {
+				break
+			}
+			//Send to server
+			_, err = con.Write([]byte(request))
+			//Valid terminated string
+			_, err = con.Write([]byte("\r\n"))
 		}
-		_, err = con.Write([]byte(message))
 		if err != nil {
-			return fmt.Errorf("Write error: %s", err)
+			return fmt.Errorf("Interact error: %s", err)
 		}
+		response = make([]byte, 8)
 	}
 	defer fmt.Println("Connection terminated")
 	return con.Close()
